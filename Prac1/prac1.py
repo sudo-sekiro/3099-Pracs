95af805ea43d18f5646972068277d836fa977c40
jon <jfriedman97@gmail.com>
2019-07-29 16:11:34 +0100

    GPIO done

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -11,25 +11,30 @@ import RPi.GPIO as GPIO
 
 # Logic that you write
 def main():
-    init_GPIO()
-
+	init_GPIO()
+	while(True):
+		GPIO.output(17, GPIO.HIGH)
+		pass
 
 
 def init_GPIO():
+	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(26, GPIO.OUT)
 	GPIO.setup(17, GPIO.OUT)
 	GPIO.setup(27, GPIO.OUT)
+	GPIO.setup(6, GPIO.IN)
+	GPIO.setup(16, GPIO.IN)
 	GPIO.add_event_detect(6, GPIO.RISING, bouncetime = 300)
-	GPIO.add_event_callback(6, callback=callback1())
+	GPIO.add_event_callback(6, callback=callback1)
 	GPIO.add_event_detect(16, GPIO.RISING, bouncetime = 300)
-	GPIO.add_event_callback(16, callback=callback2())
+	GPIO.add_event_callback(16, callback=callback2)
 
 
 def callback1(channel):
-	GPIO.output(18,GPIO.HIGH)
+	GPIO.output(26,GPIO.HIGH)
 
-def callback2():
+def callback2(channel):
 	GPIO.output(17, GPIO.HIGH)
 
 # Only run the functions if 
@@ -42,7 +47,7 @@ if __name__ == "__main__":
         print("Exiting gracefully")
         # Turn off your GPIOs here
         GPIO.cleanup()
-    except Exception as e:
-        GPIO.cleanup()
-        print("Some other error occurred")
-        print(e.message)
+#    except Exception as e:
+ #       GPIO.cleanup()
+  #      print("Some other error occurred")
+   #     print(e.message)
