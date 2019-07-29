7fd1f8c3cd46ba792e01d5d32fd28df9b0826ddf
jon <jfriedman97@gmail.com>
2019-07-29 16:44:58 +0100

    switch working

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -13,21 +13,20 @@ import RPi.GPIO as GPIO
 def main():
 	init_GPIO()
 	while(True):
-		GPIO.output(17, GPIO.HIGH)
 		pass
 
 
 def init_GPIO():
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(26, GPIO.OUT)
-	GPIO.setup(17, GPIO.OUT)
-	GPIO.setup(27, GPIO.OUT)
-	GPIO.setup(6, GPIO.IN)
-	GPIO.setup(16, GPIO.IN)
-	GPIO.add_event_detect(6, GPIO.RISING, bouncetime = 300)
+	GPIO.setup(26, GPIO.OUT, initial = GPIO.LOW)
+	GPIO.setup(17, GPIO.OUT, initial = GPIO.LOW)
+	GPIO.setup(27, GPIO.OUT, initial = GPIO.LOW)
+	GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.add_event_detect(6, GPIO.FALLING, bouncetime = 300)
 	GPIO.add_event_callback(6, callback=callback1)
-	GPIO.add_event_detect(16, GPIO.RISING, bouncetime = 300)
+	GPIO.add_event_detect(16, GPIO.FALLING, bouncetime = 300)
 	GPIO.add_event_callback(16, callback=callback2)
 
 
@@ -35,7 +34,7 @@ def callback1(channel):
 	GPIO.output(26,GPIO.HIGH)
 
 def callback2(channel):
-	GPIO.output(17, GPIO.HIGH)
+	GPIO.output(27, GPIO.HIGH)
 
 # Only run the functions if 
 if __name__ == "__main__":
@@ -47,7 +46,7 @@ if __name__ == "__main__":
         print("Exiting gracefully")
         # Turn off your GPIOs here
         GPIO.cleanup()
-#    except Exception as e:
- #       GPIO.cleanup()
-  #      print("Some other error occurred")
-   #     print(e.message)
+    except Exception as e:
+        GPIO.cleanup()
+        print("Some other error occurred")
+        print(e.message)
